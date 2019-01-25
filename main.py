a9075568f3aef62d11a6f77bab0ccc2c45e7d274
Evan Nagareda <evansmac@dhcp-10-105-77-62.wireless.northwestern.private>
2019-01-25 09:38:45 -0600

    Final Commit

--- main.py
+++ main.py
@@ -53,7 +53,29 @@ class KBTest(unittest.TestCase):
         self.assertEqual(str(answer[3]), "?X : pyramid2, ?Y : green")
         self.assertEqual(str(answer[4]), "?X : pyramid3, ?Y : red")
         self.assertEqual(str(answer[5]), "?X : pyramid4, ?Y : red")
-        
+
+    def test6(self):
+        ask1 = read.parse_input("fact: (size ?X ?Y)")
+        print(' Asking if', ask1)
+        answer = self.KB.kb_ask(ask1)
+        self.assertEqual(str(answer[0]), "?X : bigbox, ?Y : big")
+        self.assertEqual(str(answer[1]), "?X : littlebox, ?Y : small")
+        self.assertEqual(str(answer[2]), "?X : pyramid1, ?Y : small")
+        self.assertEqual(str(answer[3]), "?X : pyramid2, ?Y : small")
+        self.assertEqual(str(answer[4]), "?X : pyramid3, ?Y : big")
+        self.assertEqual(str(answer[5]), "?X : pyramid4, ?Y : big")
+
+    def test7(self):
+        ask1 = read.parse_input("fact: (shape ?X ?Y)")
+        print(' Asking if', ask1)
+        answer = self.KB.kb_ask(ask1)
+        self.assertFalse(answer)
+
+    def test8(self):
+        ask1 = read.parse_input("fact: (color sphere1 ?Y)")
+        print(' Asking if', ask1)
+        answer = self.KB.kb_ask(ask1)
+        self.assertFalse(answer)
 
 if __name__ == '__main__':
     unittest.main()
